cb7c8d9322f2b0ef1ea977cbd3cbb65567dcce1a
daanzu <daanzu@gmail.com>
2019-07-05 04:33:21 -0400

    finish preliminary lexicon adding

--- kaldi_active_grammar/__main__.py
+++ kaldi_active_grammar/__main__.py
@@ -9,9 +9,9 @@ import logging, os.path, shutil
 import six
 
 from . import _log, _name
-from .utils import debug_timer, find_file
+from .utils import debug_timer
 from .compiler import Compiler
-from .kaldi import augment_phones_txt, augment_words_txt
+from .model import Model, convert_generic_model_to_agf
 
 def compile_dictation_graph(model_dir, tmp_dir, g_filepath=None):
     compiler = Compiler(model_dir, tmp_dir)
@@ -19,80 +19,28 @@ def compile_dictation_graph(model_dir, tmp_dir, g_filepath=None):
     with debug_timer(six.print_, "graph compilation", independent=True):
         compiler.compile_dictation_fst(g_filepath)
 
-def convert_generic_model_to_agf(src_dir, model_dir):
-    filenames = [
-        'words.txt',
-        'phones.txt',
-        'align_lexicon.int',
-        'disambig.int',
-        # 'L_disambig.fst',
-        'tree',
-        'final.mdl',
-        'lexiconp.txt',
-        'word_boundary.txt',
-        'optional_silence.txt',
-        'silence.txt',
-        'nonsilence.txt',
-        'wdisambig_phones.int',
-        'wdisambig_words.int',
-        'mfcc_hires.conf',
-        'mfcc.conf',
-        'ivector_extractor.conf',
-        'splice.conf',
-        'online_cmvn.conf',
-        'final.mat',
-        'global_cmvn.stats',
-        'final.dubm',
-        'final.ie',
-    ]
-    nonterminals = list(Compiler.nonterminals)
-
-    for filename in filenames:
-        path = find_file(src_dir, filename)
-        if path is None:
-            _log.error("cannot find %r in %r", filename, model_dir)
-            continue
-        _log.info("copying %r to %r", path, model_dir)
-        shutil.copy(path, model_dir)
-
-    _log.info("converting %r in %r", 'phones.txt', model_dir)
-    lines, highest_symbol = augment_phones_txt.read_phones_txt(os.path.join(model_dir, 'phones.txt'))
-    augment_phones_txt.write_phones_txt(lines, highest_symbol, nonterminals, os.path.join(model_dir, 'phones.txt'))
-
-    _log.info("converting %r in %r", 'words.txt', model_dir)
-    lines, highest_symbol = augment_words_txt.read_words_txt(os.path.join(model_dir, 'words.txt'))
-    # FIXME: leave space for adding words later
-    augment_words_txt.write_words_txt(lines, highest_symbol, nonterminals, os.path.join(model_dir, 'words.txt'))
-
-    with open(os.path.join(model_dir, 'nonterminals.txt'), 'wb') as f:
-        f.writelines(nonterm + '\n' for nonterm in nonterminals)
-
-    # add nonterminals to align_lexicon.int
-    
-    # fix L_disambig.fst
-
 def main():
     import argparse
     parser = argparse.ArgumentParser(prog='python -m %s' % _name)
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-m', '--model_dir')
     parser.add_argument('-t', '--tmp_dir')
-    parser.add_argument('command', choices=['compile_dictation_graph', 'convert_generic_model_to_agf'])
-    parser.add_argument('file', nargs='?')
-    # parser.add_argument('file')
+    parser.add_argument('command', choices=['compile_dictation_graph', 'convert_generic_model_to_agf', 'add_word'])
     # FIXME: helps
     # FIXME: subparsers?
-    # args = parser.parse_args()
     args, unknown = parser.parse_known_args()
-    if not args.file and unknown: args.file = unknown.pop(0)
 
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
     if args.command == 'compile_dictation_graph':
         if not args.model_dir: parser.error("MODEL_DIR required for compile_dictation_graph")
-        compile_dictation_graph(args.model_dir, args.tmp_dir, args.file)
+        compile_dictation_graph(args.model_dir, args.tmp_dir, unknown[0])
     if args.command == 'convert_generic_model_to_agf':
         if not args.model_dir: parser.error("MODEL_DIR required for convert_generic_model_to_agf")
-        convert_generic_model_to_agf(args.file, args.model_dir)
+        convert_generic_model_to_agf(unknown[0], args.model_dir)
+    if args.command == 'add_word':
+        if not args.model_dir: parser.error("MODEL_DIR required for add_word")
+        Model(args.model_dir).add_word(unknown[0], unknown[1].split())
 
 if __name__ == '__main__':
     main()
--- kaldi_active_grammar/model.py
+++ kaldi_active_grammar/model.py
@@ -4,7 +4,11 @@
 # Licensed under the AGPL-3.0, with exceptions; see LICENSE.txt file.
 #
 
-import re
+import os, re
+
+from .utils import find_file, load_symbol_table
+from .compiler import Compiler
+from .kaldi import augment_phones_txt, augment_words_txt
 
 
 class Lexicon(object):
@@ -53,28 +57,92 @@ class Lexicon(object):
     }
     CMU_to_XSAMPA_dict.update({'AX': '@'})
     del CMU_to_XSAMPA_dict["'"]
-    XSAMPA_to_CMU_dict = { v: k for v,k in CMU_to_XSAMPA_dict.items() }
-    cmu_stress_regex = re.compile(r'(0|1|2)$')
+    XSAMPA_to_CMU_dict = { v: k for k,v in CMU_to_XSAMPA_dict.items() }  # FIXME: handle double-entries
 
     @classmethod
     def cmu_to_xsampa(cls, phones):
-        # cmu_stress_repl = lambda matchobj: "'" if matchobj.group() == '1' else ''
-        def repl(phone):
+        new_phones = []
+        for phone in phones:
             stress = False
             if phone.endswith('1'):
                 phone = phone[:-1]
                 stress = True
             elif phone.endswith(('0', '2')):
                 phone = phone[:-1]
-            return ("'" if stress else '') + cls.CMU_to_XSAMPA_dict[phone]
-        # return [cls.CMU_to_XSAMPA_dict[cls.cmu_stress_regex.sub(cmu_stress_repl, phone)] for phone in phones]
-        return [repl(phone) for phone in phones]
+            phone = cls.CMU_to_XSAMPA_dict[phone]
+            assert 1 <= len(phone) <= 2
+            while len(phone):
+                new_phones.append(("'" if stress else '') + phone[:1])
+                stress = False
+                phone = phone[1:]
+        return new_phones
+
+    @classmethod
+    def make_position_dependent(cls, phones):
+        if len(phones) == 0: return []
+        elif len(phones) == 1: return [phones[0]+'_S']
+        else: return [phones[0]+'_B'] + [phone+'_I' for phone in phones[1:-1]] + [phones[-1]+'_E']
 
 
 class Model(object):
     def __init__(self, model_dir):
-        self.dir = os.path.join(model_dir, '')
+        self.model_dir = os.path.join(model_dir, '')
+        self.phone_to_int_dict = { phone: i for phone, i in load_symbol_table(os.path.join(self.model_dir, 'phones.txt')) }
 
-    @staticmethod
-    def add_word(word, phones):
+    def add_word(self, word, phones):
         phones = Lexicon.cmu_to_xsampa(phones)
+        phones = Lexicon.make_position_dependent(phones)
+        phone_ids = [self.phone_to_int_dict[phone] for phone in phones]
+
+
+def convert_generic_model_to_agf(src_dir, model_dir):
+    filenames = [
+        'words.txt',
+        'phones.txt',
+        'align_lexicon.int',
+        'disambig.int',
+        # 'L_disambig.fst',
+        'tree',
+        'final.mdl',
+        'lexiconp.txt',
+        'word_boundary.txt',
+        'optional_silence.txt',
+        'silence.txt',
+        'nonsilence.txt',
+        'wdisambig_phones.int',
+        'wdisambig_words.int',
+        'mfcc_hires.conf',
+        'mfcc.conf',
+        'ivector_extractor.conf',
+        'splice.conf',
+        'online_cmvn.conf',
+        'final.mat',
+        'global_cmvn.stats',
+        'final.dubm',
+        'final.ie',
+    ]
+    nonterminals = list(Compiler.nonterminals)
+
+    for filename in filenames:
+        path = find_file(src_dir, filename)
+        if path is None:
+            _log.error("cannot find %r in %r", filename, model_dir)
+            continue
+        _log.info("copying %r to %r", path, model_dir)
+        shutil.copy(path, model_dir)
+
+    _log.info("converting %r in %r", 'phones.txt', model_dir)
+    lines, highest_symbol = augment_phones_txt.read_phones_txt(os.path.join(model_dir, 'phones.txt'))
+    augment_phones_txt.write_phones_txt(lines, highest_symbol, nonterminals, os.path.join(model_dir, 'phones.txt'))
+
+    _log.info("converting %r in %r", 'words.txt', model_dir)
+    lines, highest_symbol = augment_words_txt.read_words_txt(os.path.join(model_dir, 'words.txt'))
+    # FIXME: leave space for adding words later
+    augment_words_txt.write_words_txt(lines, highest_symbol, nonterminals, os.path.join(model_dir, 'words.txt'))
+
+    with open(os.path.join(model_dir, 'nonterminals.txt'), 'wb') as f:
+        f.writelines(nonterm + '\n' for nonterm in nonterminals)
+
+    # add nonterminals to align_lexicon.int
+    
+    # fix L_disambig.fst: construct lexiconp_disambig.txt ...
