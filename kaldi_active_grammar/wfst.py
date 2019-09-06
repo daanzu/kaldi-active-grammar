5e74de261cc224f8f65f26255912d628655894a8
daanzu <daanzu@gmail.com>
2019-09-06 02:39:50 -0400

    optimize WFST parsing more

--- kaldi_active_grammar/wfst.py
+++ kaldi_active_grammar/wfst.py
@@ -4,9 +4,9 @@
 # Licensed under the AGPL-3.0, with exceptions; see LICENSE.txt file.
 #
 
-import collections, math
+import collections, itertools, math
 
-from six import iteritems
+from six import iteritems, itervalues
 
 class WFST(object):
     zero = float('inf')  # weight of non-final states
@@ -18,15 +18,18 @@ class WFST(object):
     def __init__(self):
         self.clear()
 
-    num_arcs = property(lambda self: len(self._arc_table))
-    num_states = property(lambda self: len(self._state_table))
-
     def clear(self):
-        self._arc_table = []  # [src_state, dst_state, label, olabel, weight]
+        self._arc_table_dict = collections.defaultdict(list)  # {src_state: [[src_state, dst_state, label, olabel, weight], ...]}  # list of its outgoing arcs
         self._state_table = dict()  # {id: weight}
         self._state_to_num_arcs = collections.Counter()
         self._next_state_id = 0
         self.start_state = self.add_state()
+
+    num_arcs = property(lambda self: sum(len(arc_list) for arc_list in itervalues(self._arc_table_dict)))
+    num_states = property(lambda self: len(self._state_table))
+
+    def iter_arcs(self):
+        return itertools.chain.from_iterable(itervalues(self._arc_table_dict))
 
     def add_state(self, weight=None, initial=False, final=False):
         id = self._next_state_id
@@ -44,13 +47,13 @@ class WFST(object):
         if label is None: label = self.eps
         if olabel is None: olabel = label
         weight = self.one if weight is None else -math.log(weight)
-        self._arc_table.append([src_state, dst_state, label, olabel, weight])
+        self._arc_table_dict[src_state].append([src_state, dst_state, label, olabel, weight])
         self._state_to_num_arcs[src_state] += 1
 
     def get_fst_text(self, eps2disambig=False):
         eps_replacement = self.eps_disambig if eps2disambig else self.eps
         text = ''.join("%s %s %s %s %s\n" % (src_state, dst_state, str(ilabel) if ilabel != self.eps else eps_replacement, str(olabel), weight)
-            for (src_state, dst_state, ilabel, olabel, weight) in self._arc_table)
+            for (src_state, dst_state, ilabel, olabel, weight) in self.iter_arcs())
         text += ''.join("%s %s\n" % (id, weight) for (id, weight) in iteritems(self._state_table) if weight is not self.zero)
         return text
 
@@ -64,7 +67,7 @@ class WFST(object):
 
     def equalize_weights(self):
         # breakeven 10-13?
-        for arc in self._arc_table:
+        for arc in self.iter_arcs():
             arc[4] = -math.log(1.0 / self._state_to_num_arcs[arc[0]])
 
     def has_eps_path(self, src_state, dst_state):
@@ -76,17 +79,13 @@ class WFST(object):
             state = state_queue.pop()
             if state == dst_state:
                 return True
-            next_states = [dst_state for (src_state, dst_state, label, olabel, weight) in self._arc_table
+            next_states = [dst_state for (src_state, dst_state, label, olabel, weight) in self.iter_arcs()
                 if src_state == state and label in eps_like and dst_state not in queued]
             state_queue.extendleft(next_states)
             queued.update(next_states)
         return False
 
     def does_match(self, target_words, include_silent=False):
-        arc_table_dict = collections.defaultdict(list)  # optimization: maps src_state -> a list of its outgoing arcs
-        for arc in self._arc_table:
-            arc_table_dict[arc[0]].append(arc)
-
         queue = collections.deque()  # entries: (state, path of arcs to state, index of remaining words)
         queue.append((self.start_state, (), 0))
         while queue:
@@ -94,7 +93,7 @@ class WFST(object):
             if (target_word_index >= len(target_words)) and self.state_is_final(state):
                 return tuple(ilabel for (src_state, dst_state, ilabel, olabel, weight) in path
                     if include_silent or (ilabel not in self.silent_labels))
-            for arc in arc_table_dict[state]:
+            for arc in self._arc_table_dict[state]:
                 src_state, dst_state, ilabel, olabel, weight = arc
                 assert src_state == state
                 if ilabel == target_words[target_word_index]:
