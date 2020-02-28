411b59a1a980869d7cfe05e2b1c5418cc5f3452b
daanzu <daanzu@gmail.com>
2020-02-28 06:27:07 -0500

    improve plain dictation example

--- examples/plain_dictation.py
+++ examples/plain_dictation.py
@@ -2,7 +2,8 @@ import sys, wave
 from kaldi_active_grammar import PlainDictationRecognizer
 
 recognizer = PlainDictationRecognizer()  # Or supply non-default model_dir, tmp_dir, or fst_file
-wave_file = wave.open(sys.argv[1], 'rb')
+filename = sys.argv[1] if len(sys.argv) > 1 else 'test.wav'
+wave_file = wave.open(filename, 'rb')
 data = wave_file.readframes(wave_file.getnframes())
 output_str, likelihood = recognizer.decode_utterance(data)
 print(repr(output_str), likelihood)  # -> 'alpha bravo charlie' 1.1923989057540894
