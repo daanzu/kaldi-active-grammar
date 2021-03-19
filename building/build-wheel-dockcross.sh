f8a98c813e0a6d52d8e940126544c8de48c62524
daanzu <daanzu@gmail.com>
2021-03-19 08:14:09 -0400

    simplify building with openblas on linux/macos, and make it the default

--- building/build-wheel-dockcross.sh
+++ building/build-wheel-dockcross.sh
@@ -5,8 +5,7 @@ set -e -x
 PYTHON_EXE=/opt/python/cp38-cp38/bin/python
 WHEEL_PLAT=$1
 KALDI_BRANCH=$2
-MKL_URL=${3:-"https://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16917/l_mkl_2020.4.304.tgz"}
-MKL_FILE=$(basename $MKL_URL)
+MKL_URL=$3
 
 if [ -z "$WHEEL_PLAT" ] || [ -z "$PYTHON_EXE" ]; then
     echo "ERROR: variable not set!"
@@ -17,14 +16,18 @@ mkdir -p _skbuild
 rm -rf _skbuild/*/cmake-install/ _skbuild/*/setuptools/
 rm -rf kaldi_active_grammar/exec
 
-pushd _skbuild
-wget --no-verbose --no-clobber $MKL_URL
-mkdir -p /tmp/mkl
-tar zxf $MKL_FILE -C /tmp/mkl --strip-components=1
-sed -i.bak -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' -e 's/ARCH_SELECTED=ALL/ARCH_SELECTED=INTEL64/g' /tmp/mkl/silent.cfg
-sudo /tmp/mkl/install.sh --silent /tmp/mkl/silent.cfg
-rm -rf /tmp/mkl
-popd
+if [ -n "$MKL_URL" ]; then
+    pushd _skbuild
+    wget --no-verbose --no-clobber $MKL_URL
+    mkdir -p /tmp/mkl
+    MKL_FILE=$(basename $MKL_URL)
+    tar zxf $MKL_FILE -C /tmp/mkl --strip-components=1
+    sed -i.bak -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' -e 's/ARCH_SELECTED=ALL/ARCH_SELECTED=INTEL64/g' /tmp/mkl/silent.cfg
+    sudo /tmp/mkl/install.sh --silent /tmp/mkl/silent.cfg
+    rm -rf /tmp/mkl
+    export INTEL_MKL_DIR="/opt/intel/mkl/"
+    popd
+fi
 
 # $PYTHON_EXE -m pip install --upgrade setuptools wheel scikit-build cmake ninja
 KALDI_BRANCH=$KALDI_BRANCH $PYTHON_EXE setup.py bdist_wheel
