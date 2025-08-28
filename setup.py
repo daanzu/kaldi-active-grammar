030445c92764db1252a1935cbae5ac48e8b1093a
David Zurow <daanzu@gmail.com>
2025-08-28 04:20:19 -0400

    Update setup classifiers for supported python versions

--- setup.py
+++ setup.py
@@ -180,6 +180,10 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
     ],
 
     # This field adds keywords for your project which will appear on the
