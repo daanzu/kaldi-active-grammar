01cf15d81bf7ea1abd0f0a98f77aac691f302934
David Zurow <daanzu@gmail.com>
2025-11-03 11:06:03 -0500

    Update python support specifiers, dropping 3.6-3.8, but still allowing installs

--- setup.py
+++ setup.py
@@ -173,9 +173,6 @@ setup(
         # These classifiers are *not* checked by 'pip install'. See instead
         # 'python_requires' below.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
@@ -207,7 +204,7 @@ setup(
     # and refuse to install the project if the version does not match. If you
     # do not support Python 2, you can simplify this to '>=3.5' or similar, see
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.6, <4',
+    python_requires='>=3.6, <4',  # NOTE: Allowing earlier unsupported versions, even if not tested, unless we know they break
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
